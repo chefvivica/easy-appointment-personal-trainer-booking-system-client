c190a0a5edf4e1298999831999c99389c6313542
Vivica <Ma>
2020-09-19 01:56:22 -0400

    trainer login

--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -9,9 +9,10 @@ export class NavBar extends Component {
   return(
     <div className="nav-bar">
     <NavLink to="/calendar" exact>Calendar</NavLink>
-    <NavLink to="/trainer" exact>Trainer</NavLink>
+    <NavLink to="/trainer" exact>All Trainers</NavLink>
     <NavLink to="/users/:id" exact>Your Profile</NavLink>
     <NavLink to="/" exact>Login</NavLink>
+    <NavLink to="/trainer/login/:id" exact id="trainer-login"><button>Trainer Login</button></NavLink>
     </div>
 
     )
