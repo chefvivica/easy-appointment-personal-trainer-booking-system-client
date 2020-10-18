95aa9cdb792c678459f3dc8e0fee517f5d6266d8
Vivica <Ma>
2020-10-18 01:02:57 -0400

    .

--- src/container/TrainerProfile.js
+++ src/container/TrainerProfile.js
@@ -7,6 +7,7 @@ export default class TrainerProfile extends Component {
         <ul>
           <li>your name</li>
           <li>email</li>
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTkocCs_1yy_4OWkQBsdE75ZWmdMAby_ctc1w&usqp=CAU" />
         </ul>
       </div>
     )
